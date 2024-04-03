c1f56b4b587f3227174726a85e4bbc2bb45764c2
Joerg Lempe <joerg.lempe@gmx.de>
2024-04-03 19:22:42 +0200

    neue Farben

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter><div>Text</div></BrowserRouter>
+    <BrowserRouter>
+      <div>Hier kommt Inhalt</div>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "fitness-color-dark": "#202430",
+        "fitness-color-medium": "#3A4151",
+        "fitness-color-light": "#202430",
+        "fitness-color-grandis": "#FFCC6A",
+        "fitness-color-light-pink": "#FE9EB9",
+        "fitness-color-turkis": "#4CF4E2",
+        "fitness-color-mint": "#EDFDA7",
+        "fitness-color-blue": "#3B5FE5",
+      },
+    },
   },
   plugins: [],
 };
