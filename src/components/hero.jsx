b382c8b1c86f6a2e1124814de288854f583f8cdc
Joerg Lempe <joerg.lempe@gmx.de>
2024-04-04 15:13:42 +0200

    Anfang Startseite

---
+++ src/components/hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import heropic from "../../img/heropic.png";
+
+function Hero() {
+  return (
+    <div>
+      <img src={heropic} alt="Training" />
+    </div>
+  );
+}
+
+export default Hero;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import Greeting from "/components/greeting";
+import Hero from "/components/hero";
+import NavBar from "/components/navibar";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <div>Hier kommt Inhalt</div>
+      <div>
+        <Greeting />
+        <Hero />
+        <NavBar />
+      </div>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
